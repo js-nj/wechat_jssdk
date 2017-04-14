bcabd0de0c04249defff99ec5eccf22d082d5505
zhuhui <huizhu@wisedu.com>
2017-04-14 17:51:26 +0800

    添加content_type  允许跨域

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.locals._layoutFile = 'layout.html';
 
 app.all('*', function(req, res, next) {  
     res.header("Access-Control-Allow-Origin", "*");  
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");  
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
     res.header("X-Powered-By",' 3.2.1')  
     res.header("Content-Type", "application/json;charset=utf-8");  
